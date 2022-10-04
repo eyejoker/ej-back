088ffe0e2760f8050065f851fdc23113eba56d4d
ww232330 <ww232330@gmail.com>
2022-10-04 21:51:13 +0900

    ✅ update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, DatabaseModule],
   controllers: [AppController],
   providers: [],
 })
